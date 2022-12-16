3f19d18b4e1f763e0378ccb3df22d7aac9164efd
Adarsh <adarshkumar167@gmail.com>
2022-12-16 18:42:56 +0530

    2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Harry
+          Learn React with Adarsh
         </a>
       </header>
     </div>
