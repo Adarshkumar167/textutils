a916a969a4730d308ab2e15313e1eb541f217612
Adarsh <adarshkumar167@gmail.com>
2022-12-16 19:19:18 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
+let name = 'Adarsh';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Adarsh
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing 
+        elit. Nemo commodi corporis perspiciatis suscipit 
+        inventore nobis architecto dolorum deserunt, cum 
+        quae neque quis hic natus rem alias quos minima 
+        repellat magnam corrupti molestias ducimus modi!
+      </p>
     </div>
+    </>
+    
   );
 }
 
